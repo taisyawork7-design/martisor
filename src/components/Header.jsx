3040fd8ae63ea2eaf81107900db097086c5e7424
DESKTOP-JKPICFM\user <taia@x-cloud.info>
2025-12-17 12:17:17 +0200

    first

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,87 +1,115 @@
-import React, { useEffect, useState } from 'react'
-import { useTranslation } from 'react-i18next'
-import i18n from "../../i18n"
-import arrow from "../images/arrow.svg"
+import React, { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import i18n from "../../i18n";
+import arrow from "../images/arrow.svg";
 import { TbMenu2 } from "react-icons/tb";
+import martisor from "../images/martisor.svg";
 
 export default function Header({ onLanguageChange }) {
-    const { t } = useTranslation('header')
-    const [selectedLanguage, setSelectedLanguage] = useState(localStorage.getItem('selectedLanguage') || "ru")
-    const [dropdownOpen, setDropdownOpen] = useState(false)
-    const [openMenu, setOpenMenu] = useState(false)
+  const { t } = useTranslation("header");
+  const [selectedLanguage, setSelectedLanguage] = useState(
+    localStorage.getItem("selectedLanguage") || "ru"
+  );
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false);
 
-    useEffect(() => {
-        i18n.changeLanguage(selectedLanguage)
-    }, [selectedLanguage])
+  useEffect(() => {
+    i18n.changeLanguage(selectedLanguage);
+  }, [selectedLanguage]);
 
-    const changeLanguage = (lang) => {
-        setSelectedLanguage(lang)
-        localStorage.setItem("selectedLanguage", lang)
-        onLanguageChange(lang)
-        setDropdownOpen(false)
+  const changeLanguage = (lang) => {
+    setSelectedLanguage(lang);
+    localStorage.setItem("selectedLanguage", lang);
+    onLanguageChange(lang);
+    setDropdownOpen(false);
+  };
+  const languages = { ro: "Ro", ru: "Ру", en: "En" };
+  const dropdownOptions = Object.entries(languages).filter(
+    ([key]) => key !== selectedLanguage
+  );
+  useEffect(() => {
+    if (openMenu) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
     }
-    const languages = { ro: "Ro", ru: "Ру", en: "En" }
-    const dropdownOptions = Object.entries(languages).filter(([key]) => key !== selectedLanguage)
-    useEffect(() => {
-        if (openMenu) {
-            document.body.style.overflow = "hidden"
-        } else {
-            document.body.style.overflow = ""
-        }
-    
-        return () => {
-            document.body.style.overflow = ""
-        }
-    }, [openMenu])
-    
-    return (
-        <header>
-            <h1>WORK<span>TONE</span></h1>
-            <nav>
-                <a href="#about">{t("about")}</a>
-                <a href="#services">{t("services")}</a>
-                <a href="#packages">{t("packages")}</a>
-                <a href="#contacts">{t("contacts")}</a>
-            </nav>
-            <div className="change-language">
-                <button onClick={() => setDropdownOpen(!dropdownOpen)} >{languages[selectedLanguage]} <img src={arrow} alt="" /> </button>
-                {dropdownOpen && (
-                    <div className="dropdown">
-                        {dropdownOptions.map(([key, label]) => (
-                            <p key={key} onClick={() => changeLanguage(key)}>
-                                {label}
-                            </p>
-                        ))}
-                    </div>
-                )}
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [openMenu]);
+
+  return (
+    <header>
+      <div className="logo">
+        <img src={martisor} alt="" />
+        <h1>
+          {" "}
+          MĂRȚIȘOR:{" "}
+          <p>
+            RED & <span>WHITE</span>{" "}
+          </p>
+        </h1>
+      </div>
+      <nav>
+        <a href="#about">{t("about")}</a>
+        <a href="#artists">{t("artist")}</a>
+        <a href="#packages">{t("activity")}</a>
+        <a href="#contacts">{t("location")}</a>
+        <a href="#contacts">+37376574645</a>
+      </nav>
+      <div className="change-language">
+        <button onClick={() => setDropdownOpen(!dropdownOpen)}>
+          {languages[selectedLanguage]} <img src={arrow} alt="" />{" "}
+        </button>
+        {dropdownOpen && (
+          <div className="dropdown">
+            {dropdownOptions.map(([key, label]) => (
+              <p key={key} onClick={() => changeLanguage(key)}>
+                {label}
+              </p>
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="burger-menu">
+        <button onClick={() => setOpenMenu(!openMenu)}>
+          <TbMenu2 />
+        </button>
+        {openMenu && (
+          <div className="open-menu">
+            <div className="menu">
+              <h1>
+                WORK<span>TONE</span>
+              </h1>
+              <a href="#about" onClick={() => setOpenMenu(false)}>
+                {t("about")}
+              </a>
+              <a href="#services" onClick={() => setOpenMenu(false)}>
+                {t("services")}
+              </a>
+              <a href="#packages" onClick={() => setOpenMenu(false)}>
+                {t("packages")}
+              </a>
+              <a href="#contacts" onClick={() => setOpenMenu(false)}>
+                {t("contacts")}
+              </a>
+              <div className="mobile-dropdown">
+                {Object.keys(languages).map((lang) => (
+                  <p
+                    key={lang}
+                    onClick={() => changeLanguage(lang)}
+                    className={i18n.language === lang ? "active" : ""}
+                  >
+                    {languages[lang]}
+                  </p>
+                ))}
+              </div>
             </div>
-            <div className="burger-menu">
-                <button onClick={() => setOpenMenu(!openMenu)} ><TbMenu2 /></button>
-                {openMenu && (
-                    <div className="open-menu">
-                      
-                        <div className="menu">
-                            <h1>WORK<span>TONE</span></h1>
-                            <a href="#about" onClick={() => setOpenMenu(false)}>{t("about")}</a>
-                            <a href="#services" onClick={() => setOpenMenu(false)}>{t("services")}</a>
-                            <a href="#packages" onClick={() => setOpenMenu(false)}>{t("packages")}</a>
-                            <a href="#contacts" onClick={() => setOpenMenu(false)}>{t("contacts")}</a>
-                            <div className="mobile-dropdown">
-                                {Object.keys(languages).map((lang) => (
-                                    <p
-                                        key={lang}
-                                        onClick={() => changeLanguage(lang)}
-                                        className={i18n.language === lang ? "active" : ""}
-                                    >
-                                        {languages[lang]}
-                                    </p>
-                                ))}
-                            </div>
-                        </div>
-                        <div className="overlay" onClick={() => setOpenMenu(false)}></div>
-                    </div>
-                )}
-            </div>
-        </header>
-    )
+            <div className="overlay" onClick={() => setOpenMenu(false)}></div>
+          </div>
+        )}
+      </div>
+    </header>
+  );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,111 +1,117 @@
-import React, { useState, useEffect } from 'react'
-import { useTranslation } from 'react-i18next'
-import card1 from "../images/analytics.jpg"
-import card2 from "../images/anonim.jpg"
-import card3 from "../images/exc.jpg"
-import card4 from "../images/quest.jpg"
-import card5 from "../images/vr.jpg"
-import phone from "../images/phone.svg"
-import location from "../images/location.svg"
-import email from "../images/email.svg"
-import inst from "../images/instagram.svg"
-import telegram from "../images/telegram.svg"
-import tik from "../images/tik.svg"
-import SimpleForm from './Form'
-import SliderAbout from './SliderAbout'
-import SliderServicies from './SliderServicies'
-
-
+import React, { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import phone from "../images/phone.svg";
+import location from "../images/location.svg";
+import email from "../images/email.svg";
+import inst from "../images/instagram.svg";
+import telegram from "../images/telegram.svg";
+import tik from "../images/tik.svg";
+import SimpleForm from "./Form";
+import SliderAbout from "./SliderAbout";
+import SliderServicies from "./SliderServicies";
+import left from "../images/left.svg";
+import right from "../images/right.svg";
+import carpetLine from "../images/carpetLine.png";
+import sticker from "../images/sticker.svg";
 
 export default function Home() {
-  const { t } = useTranslation('main');
-
+  const { t } = useTranslation("main");
 
   return (
     <main>
       <div className="banner">
-        <h1>WORK<span>TONE</span></h1>
-        <p>{t('bannerText')}</p>
+        <img src={left} alt="" />
+        <div className="banner__text">
+          <div className="banner__text__date">
+            <p>28.02 - 1.03</p>
+            <h2>{t("bannerDate")}</h2>
+            <h3>13:00-23:00</h3>
+          </div>
+          <div className="banner__text__info">
+            <h1>
+              MĂRȚIȘOR:{" "}
+              <p>
+                RED & <span>WHITE</span>{" "}
+              </p>
+            </h1>
+            <h2>Feel the Pulse of Spring & Tradition</h2>
+            <a href="">Great National Assembly Square, Chișinau</a>
+          </div>
+        </div>
+        <img src={right} alt="" />
       </div>
-      <div className="about" id='about'>
-        <h1>{t('aboutH1')}</h1>
-        <div className="info">
-          <SliderAbout />
-          <div className="text">
-            <p><span> WORKTONE </span>{t('aboutp1')}</p>
-            <p>{t('aboutp2')}<span> WORKTONE</span>{t('aboutp3')}</p>
-            <p></p>
+      <div className="line">
+        <img src={carpetLine} alt="" />
+      </div>
+      <div className="about" id="about">
+        <div className="about__header">
+          <img src={sticker} alt="" />
+          <div className="about__header__text">
+            <h1>
+              {t("aboutH1")} MĂRȚIȘOR: <span>RED</span> & WHITE ?
+            </h1>
+            <p>{t("aboutp1")}</p>
+          </div>
+        </div>
+        <div className="about__statistic">
+          <div className="about__statistic__stat">
+            <h2>2</h2>
+            <div className="about__statistic__stat__line"></div>
+            <p>{t("stat1")}</p>
+          </div>
+          <div className="about__statistic__stat">
+            <h2>10</h2>
+            <div className="about__statistic__stat__line"></div>
+            <p>{t("stat2")}</p>
+          </div>
+          <div className="about__statistic__stat">
+            <h2>20+</h2>
+            <div className="about__statistic__stat__line"></div>
+            <p>{t("stat3")}</p>
+          </div>
+          <div className="about__statistic__stat">
+            <h2>3</h2>
+            <div className="about__statistic__stat__line"></div>
+            <p>{t("stat4")}</p>
+          </div>
+          <div className="about__statistic__stat">
+            <h2>2+</h2>
+            <div className="about__statistic__stat__line"></div>
+            <p>{t("stat5")}</p>
           </div>
         </div>
       </div>
-      <div className="services" id='services'>
-        <h1>{t('servicesTitle')}</h1>
-      <SliderServicies/>
-      </div>
-      <div className="packages" id='packages'>
-        <h1>{t('packagesH1')}</h1>
-        <div className="dates">
-          <div className="data">
-          <h1>{t('data1H1')} <span>{t('data1span')}</span></h1>
-            <div className="text">
-            
-           
-            <p>{t('data1p1')}</p>
-            <p>{t('data1p2')}</p>
-            <p>{t('data1p3')}</p>
-            <p>{t('data1p4')}</p>
-            
-            <h3>29 €<span>{t('dataPrice')}</span> </h3>
-            </div>
-          </div>
-        
-          <div className="data">
-          <h1>{t('data2H1')} <span>{t('data1span')}</span></h1>
-            <div className="text">
-            
-            
-            <p>{t('data2p1')}</p>
-            <p>{t('data2p2')}</p>
-            <p>{t('data2p3')}</p>
-            <p>{t('data2p4')}</p>
-           
-            <h3>49 €<span> {t('dataPrice')}</span> </h3>
-            </div>
-          </div>
-        
-          <div className="data">
-          <h1>{t('data3H1')} <span>{t('data1span')}</span></h1>
-            <div className="text">
-           
-        
-            <p>{t('data3p1')}</p>
-            <p>{t('data3p2')}</p>
-            <p>{t('data3p3')}</p>
-            <p>{t('data3p4')}</p>
-            <p>{t('data3p5')}</p>
-           
-            <h3>99 € <span>{t('dataPrice')}</span> </h3>
-            </div>
-          </div>
-        
-        </div>
-      </div>
-      <div className="contact-form" id='contacts'>
-        
+      <div className="artists" id="artists"></div>
+
+      <div className="contact-form" id="contacts">
         <div className="information">
-        
           <div className="contacts">
-          <h1>{t('titleForm')}</h1>
-            <a href=""><img src={phone} alt="" />+373 876 456</a>
-            <a href=""><img src={location} alt="" />{t('addres')}</a>
-            <a href=""><img src={email} alt="" />worktone2025@gmail.com</a>
+            <h1>{t("titleForm")}</h1>
+            <a href="">
+              <img src={phone} alt="" />
+              +373 876 456
+            </a>
+            <a href="">
+              <img src={location} alt="" />
+              {t("addres")}
+            </a>
+            <a href="">
+              <img src={email} alt="" />
+              worktone2025@gmail.com
+            </a>
             <div className="media">
-              <a href=""><img src={inst} alt="" /></a>
-              <a href=""><img src={telegram} alt="" /></a>
-              <a href=""><img src={tik} alt="" /></a>
+              <a href="">
+                <img src={inst} alt="" />
+              </a>
+              <a href="">
+                <img src={telegram} alt="" />
+              </a>
+              <a href="">
+                <img src={tik} alt="" />
+              </a>
             </div>
           </div>
-        <SimpleForm />
+          <SimpleForm />
         </div>
       </div>
     </main>
